3ff185f1db8e8f9f2d17ba12da402c4ffd637269
ortegatemariangel <mariangelortega@colegioguanenta.edu.co>
2025-02-06 17:57:38 -0500

    actualizacion

--- Minicalculadora_Guanentina.py
+++ Minicalculadora_Guanentina.py
@@ -1,40 +1,42 @@
-#### Output
-print("Calculadora.py")
-print("Operadores")
-print("==========================================")
-y = int(input("Introducir el primer numero: "))
-print("==========================================")
-x = int(input("Introducir el segundo numero: "))
-print("==========================================")
-
-print("Selecciona una operación:")
-print("1. Sumar")
-print("2. Restar")
-print("3. Multiplicar")
-print("4. Dividir")
-print("5. Potencia")
-
-instruccion = int(input("Con que operador quieres (?) "))
+import math as mt
 
 
-if instruccion >= 7:
-    print("Valor no valido.")
+print("----------------------------------")
+print("1) suma")
+print("2) resta")
+print("3) multiplicación")
+print("4) división")
+print("5) potenciación")
+print("6) logaritmos")
+print("-----------------------------------")
+
+opc = int(input("digite el número correspondiente a la operacion: "))
+    
+x = int(input("digite el primer número de la operación: "))
+y = int(input("digite el segundo número de la operación: "))
+
+
+if opc == 1:
+    print(f"el resultado de la suma es {(x)+(y)}")
+
+elif opc == 2:
+    print(f"el resultado de la resta es {(x)-(y)}")
+
+elif opc == 3:
+    print(f"el resultado de la multiplicación es {(x)*(y)}")
+
+elif opc == 4:
+    if y <= 0:
+        print("Error.")
+    else:
+        print(f"el resultado de la división es {(x)/(y)}")
+
+elif opc == 5:
+    print(f"el resultado de la potenciación es {(x)**(y)}")
+
+elif opc == 6:
+    logaritmo = mt.log(y,x)
+    print(f"el resultado del logaritmo es {logaritmo}")
+
 else:
-    print("Puedes Seguir usando el programa. ")
-
-if instruccion == 1:
-    y + x
-    print("El Valor es: ", x + y)
-
-if instruccion == 2:
-    y - x
-    print(x - y)
-if instruccion == 3:
-    y * x
-    print("El valor es: ", y * x)
-if instruccion == 4:
-    y / x
-    print("El valor es: ", y / x)
-if instruccion == 5:
-    y ** x 
-    print("El valor es: ", y ** x)
+    print("no valido")
